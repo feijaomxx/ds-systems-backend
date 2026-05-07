257cf64b7010522c8d583bcbe63ee71f85996646
Davi Emanuel <davicoutinho442@gmail.com>
2026-05-07 11:50:17 -0300

    Commit inicial - Estrutura Spring Boot

---
+++ src/main/java/com/dssystems/dssystemsbackend/DsSystemsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dssystems.dssystemsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsSystemsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsSystemsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dssystems/dssystemsbackend/DsSystemsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dssystems.dssystemsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsSystemsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
