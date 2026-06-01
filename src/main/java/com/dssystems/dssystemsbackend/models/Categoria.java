4716c54b0f38558e3ffaf4ffbee76d67666a549d
Davi Emanuel <davicoutinho442@gmail.com>
2026-06-01 14:03:45 -0300

    Correção Getter e Setter

--- src/main/java/com/dssystems/dssystemsbackend/models/Categoria.java
+++ src/main/java/com/dssystems/dssystemsbackend/models/Categoria.java
@@ -24,6 +24,7 @@ public class Categoria {
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
 
-    public Integer getTipo() { return tipo; }
-    public void setTipo(Integer tipo) { this.tipo = tipo; }
+    public String getTipo() { return tipo; }
+
+    public void setTipo(String tipo) { this.tipo = tipo; }
 }
