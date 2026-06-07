af0e8d5b3fdee2ef8876db4ceccdd0bd896971af
Davi Emanuel <davicoutinho442@gmail.com>
2026-06-07 16:43:56 -0300

    Integrando com o Angular

--- src/main/java/com/dssystems/dssystemsbackend/controllers/UsuarioController.java
+++ src/main/java/com/dssystems/dssystemsbackend/controllers/UsuarioController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/usuarios")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
